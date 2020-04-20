6f173ed97f0cc1668dbd915b7210e4d266b23975
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-04-20 12:12:05 +0700

    Re-get token when get 401

--- token_test.go
+++ token_test.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -30,4 +31,42 @@ func TestToken(t *testing.T) {
 	tok, err := client.Token.Create(ctx, &TokenCreateRequest{Username: "foo@bizflycloud.vn", Password: "xxx"})
 	require.NoError(t, err)
 	require.Equal(t, "xxx", tok.KeystoneToken)
+}
+
+func TestRetryWhenTokenExpired(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc(tokenPath, func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, http.MethodPost, r.Method)
+		resp := `
+{
+    "token": "xxx",
+    "expires_at": "2019-11-22T15:39:54.000000Z"
+}
+`
+		_, _ = fmt.Fprint(w, resp)
+	})
+
+	var l loadbalancer
+	mux.HandleFunc(l.resourcePath(), func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, http.MethodGet, r.Method)
+		token := r.Header.Get("X-Auth-Token")
+		if token != "xxx" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		resp := `
+{
+    "loadbalancers": []
+}
+`
+		_, _ = fmt.Fprint(w, resp)
+	})
+
+	client.keystoneToken = "yyy"
+	lbs, err := client.LoadBalancer.List(ctx, &ListOptions{})
+	require.NoError(t, err)
+	assert.Len(t, lbs, 0)
+	assert.Equal(t, "xxx", client.keystoneToken)
 }
