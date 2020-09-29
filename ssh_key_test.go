ce48c3480b2d840a19331a2cbf9dd8b688a5c4b7
greatbn <great_bn@yahoo.com>
2020-09-29 10:31:40 +0700

    add SSHKey interface

--- ssh_key_test.go
+++ ssh_key_test.go
@@ -52,7 +52,7 @@ func TestSSHKeyList(t *testing.T) {
 `
 		_, _ = fmt.Fprint(writer, resp)
 	})
-	sshkeys, err :=client.SSHKey.List(ctx, &ListOptions{})
+	sshkeys, err := client.SSHKey.List(ctx, &ListOptions{})
 	require.NoError(t, err)
 	assert.Len(t, sshkeys, 2)
 	assert.Equal(t, "sapd1", sshkeys[0].SSHKeyPair.Name)
@@ -63,7 +63,7 @@ func TestSSHKeyDelete(t *testing.T) {
 	setup()
 	defer teardown()
 
-	mux.HandleFunc(testlib.CloudServerURL(sshKeyBasePath + "/sapd1"), func(writer http.ResponseWriter, request *http.Request) {
+	mux.HandleFunc(testlib.CloudServerURL(sshKeyBasePath+"/sapd1"), func(writer http.ResponseWriter, request *http.Request) {
 		require.Equal(t, http.MethodDelete, request.Method)
 		resp := `
 {
@@ -72,7 +72,7 @@ func TestSSHKeyDelete(t *testing.T) {
 `
 		_, _ = fmt.Fprint(writer, resp)
 	})
-	_, err :=client.SSHKey.Delete(ctx, "sapd1")
+	_, err := client.SSHKey.Delete(ctx, "sapd1")
 	require.NoError(t, err)
 }
 
@@ -92,8 +92,8 @@ func TestSSHKeyCreate(t *testing.T) {
 `
 		_, _ = fmt.Fprint(writer, resp)
 	})
-	sshkey, err :=client.SSHKey.Create(ctx, &SSHKeyCreateRequest{
-		Name: "ssh-key-1601308814384",
+	sshkey, err := client.SSHKey.Create(ctx, &SSHKeyCreateRequest{
+		Name:      "ssh-key-1601308814384",
 		PublicKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgXX0Kdd3XKojgj7maVd3PsPApzh9n2lT2CtgcJs8jw9i3mit5SZu02QFS772Pa9VdGeSjbqxtADLRpnuigW5ii0dHBQTgWqx593Cs7QKRhyRPb88u0TFCZynRwfMRnb6qngiKoWp5TtaHuIY+7kS8SyqNVIwoCYlr9a4ePX8rwydf9crhJocgKb2LgQkdW3TBE5QAvxbruYlj201jjXFeE5BtE4QER0QyY5MqW8MAgG98N3w95pKIffhHZ4TO4A3zgpWbNn1ROproZgV+9COzZ7WYuvPWqWdLAntd9b1/lLnDrDHXa/lrefJXJVamhz4i1cfIZ/p+aFWG0a7DpL5b saphi@saphi-kma\n",
 	})
 	require.NoError(t, err)
