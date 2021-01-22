50af2cbf58ffb405f386f5beb612f3ba45580c11
tung491 <dosontung007@gmail.com>
2021-01-22 16:18:30 +0700

    fix: golint err

--- testlib/url.go
+++ testlib/url.go
@@ -27,10 +27,7 @@ func K8sURL(path string) string {
 func RegistryURL(path string) string {
 	return "/api/container-registry" + path
 }
-<<<<<<< HEAD
 
 func CDNURL(path string) string {
 	return "/api/cdn" + path
 }
-=======
->>>>>>> feat: support container registry
