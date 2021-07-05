157cc3ac92fc104761ca3631a20ecb33ab64227c
tung491 <dosontung007@gmail.com>
2021-07-05 20:16:10 +0700

    feat: support gen token for CR

--- container_registry_test.go
+++ container_registry_test.go
@@ -176,3 +176,25 @@ func TestGetImage(t *testing.T) {
 	assert.Len(t, resp.Vulnerabilities, 1)
 	assert.Equal(t, "string", resp.Vulnerabilities[0].Package)
 }
+
+func TestGenToken(t *testing.T) {
+	setup()
+	defer teardown()
+	mux.HandleFunc(testlib.RegistryURL(tokenPath), func(writer http.ResponseWriter, r *http.Request) {
+		require.Equal(t, http.MethodPost, r.Method)
+		resp := `{"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkFTNzU6UEhPRTpGNU9NOllQTDU6VDRMRDpRRVNGOjZNTkw6MkNISzpYU1lIOjVWN0E6T0hVNTpVS0dOIn0.eyJpc3MiOiJjci1obi0xLnZjY2xvdWQudm46NTAwMyIsInN1YiI6InN2dHQudHVuZ2RzQHZjY2xvdWQudm4iLCJhdWQiOiJjci1obi0xLnZjY2xvdWQudm46NTAwMiIsImlhdCI6MTYyNTQ4ODQ0NiwibmJmIjoxNjI1NDg4NDQxLCJleHAiOjE2MjU0OTIwNDksImp0aSI6Ijk2NlgzOHZEbGgyU0FxdmFrWDlQWllUSGNnajQxN1gwZFlOVDRMRmtUeiIsImFjY2VzcyI6W3sicHJvamVjdF9pZCI6ImJjOGQyNzkwZmM5YTQ2OTQ5ODE4Yjk0MmMwYTgyNGRlIiwibmFtZSI6ImJjOGQyNzkwZmM5YTQ2OTQ5ODE4Yjk0MmMwYTgyNGRlL3Rlc3RfY3RsMiIsImFjdGlvbnMiOlsicHVsbCIsInB1c2giXX0seyJwcm9qZWN0X2lkIjoiYmM4ZDI3OTBmYzlhNDY5NDk4MThiOTQyYzBhODI0ZGUiLCJuYW1lIjoiYmM4ZDI3OTBmYzlhNDY5NDk4MThiOTQyYzBhODI0ZGUvamgiLCJhY3Rpb25zIjpbInB1bGwiLCJwdXNoIl19XX0.BTCK9vaRC3EAa8wPrvD1Gtrej7BzUyv3mluYT7Un-NeSUzXz7Gr7IRITyGZWH17Zt93VRsEED84yzbM1ageXsz0_nsGc4-RSESUAu980Et6z8FUj40auho4bTKuSp-bzQKFJ_90if02JYOIaB59RYpi93iiwXViUdnG3YjkJ6M5LzvnVGGXifLbGPl-m2r9bwiLfazKzpWHhVRekEfCe3OVVqTYJSYmvHFFi7Y7zVMnY_LjcVt4KHzV8vxpHdYTuX56wYrfYsXvbhzh8fa7R5RkzHyykXjTOv47sSaY5nQekiS5Q_tk7aSdjhGt1SXnkWfztpzImYHRHWFaCpUnQBQ", "expires_in": 3603, "issued_at": "2021-07-05T12:34:06+00:00"} `
+		_, _ = fmt.Fprint(writer, resp)
+	})
+	payload := GenerateTokenPayload{
+		ExpiresIn: 6969,
+		Scopes: []Scope{
+			{
+				Action: []string{"pull", "push"},
+				Repository: "test",
+			},
+		},
+	}
+	resp, err := client.ContainerRegistry.GenerateToken(ctx, &payload)
+	require.NoError(t, err)
+	assert.Equal(t, resp.ExpiresIn, 3603)
+}
