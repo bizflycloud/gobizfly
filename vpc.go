d738a6cc90da4c7e7e17bb8c8a808eec7113fe8c
dactoankmapydev <nguyendactoankma@gmail.com>
2021-07-12 18:23:32 +0700

    feat/add API get, create, update, delete, list network interface in VPC network

--- vpc.go
+++ vpc.go
@@ -10,7 +10,8 @@ import (
 )
 
 const (
-	vpcPath = "/vpc-networks"
+	vpcPath              = "/vpc-networks"
+	networkInterfacePath = "/network-interfaces"
 )
 
 var _ VPCService = (*vpcService)(nil)
@@ -25,6 +26,11 @@ type VPCService interface {
 	Update(ctx context.Context, vpcID string, uvpl *UpdateVPCPayload) (*VPC, error)
 	Create(ctx context.Context, cvpl *CreateVPCPayload) (*VPC, error)
 	Delete(ctx context.Context, vpcID string) error
+	CreateVPCNetworkInterface(ctx context.Context, networkID string, cnip *CreateNetworkInterfacePayload) (*NetworkInterface, error)
+	UpdateVPCNetworkInterface(ctx context.Context, networkID string, netInterfaceID string, unip *UpdateNetworkInterfacePayload) (*NetworkInterface, error)
+	DeleteVPCNetworkInterface(ctx context.Context, networkID string, netInterfaceID string) error
+	GetVPCNetworkInterface(ctx context.Context, networkID string, netInterfaceID string) (*NetworkInterface, error)
+	ListNetworkInterface(ctx context.Context) ([]*NetworkInterface, error)
 }
 
 type VPC struct {
@@ -75,6 +81,39 @@ type Subnet struct {
 	ProjectID       string              `json:"project_id"`
 }
 
+type NetworkInterface struct {
+	ID                  string    `json:"id"`
+	Name                string    `json:"name"`
+	NetworkID           string    `json:"network_id"`
+	TenantID            string    `json:"tenant_id"`
+	MacAddress          string    `json:"mac_address"`
+	AdminStateUp        bool      `json:"admin_state_up"`
+	Status              string    `json:"status"`
+	DeviceID            string    `json:"device_id"`
+	DeviceOwner         string    `json:"device_owner"`
+	FixedIps            []FixedIp `json:"fixed_ips"`
+	AllowedAddressPairs []string  `json:"allowed_address_pairs"`
+	ExtraDhcpOpts       []string  `json:"extra_dhcp_opts"`
+	SecurityGroups      []string  `json:"security_groups"`
+	Description         string    `json:"description"`
+	BindingVnicType     string    `json:"binding:vnic_type"`
+	PortSecurityEnabled bool      `json:"port_security_enabled"`
+	QosPolicyID         string    `json:"qos_policy_id"`
+	Tags                []string  `json:"tags"`
+	CreatedAt           string    `json:"created_at"`
+	UpdatedAt           string    `json:"updated_at"`
+	RevisionNumber      int       `json:"revision_number"`
+	ProjectID           string    `json:"project_id"`
+	AttachedServer      struct {
+	} `json:"attached_server"`
+	Firewalls []string `json:"firewalls"`
+}
+
+type FixedIp struct {
+	SubnetID  string `json:"subnet_id"`
+	IPAddress string `json:"ip_address"`
+}
+
 type CreateVPCPayload struct {
 	Name        string `json:"name"`
 	CIDR        string `json:"cidr,omitempty"`
@@ -89,8 +128,30 @@ type UpdateVPCPayload struct {
 	IsDefault   bool   `json:"is_default,omitempty"`
 }
 
+type CreateNetworkInterfacePayload struct {
+	AttachedServer string `json:"attached_server"`
+	FixedIP        string `json:"fixed_ip"`
+	Name           string `json:"name"`
+}
+
+type UpdateNetworkInterfacePayload struct {
+	Name string `json:"name"`
+}
+
+func (v vpcService) resourceNetworkInterfacePath(netInterfaceID string) string {
+	return strings.Join([]string{networkInterfacePath, netInterfaceID}, "/")
+}
+
 func (v vpcService) resourcePath() string {
 	return vpcPath
+}
+
+func (v vpcService) resourceCreateVPCNetworkInterfacePath(networkID string) string {
+	return strings.Join([]string{vpcPath, networkID, "network-interfaces"}, "/")
+}
+
+func (v vpcService) resourceVPCNetworkInterfacePath(networkID string, netInterfaceID string) string {
+	return strings.Join([]string{vpcPath, networkID, "network-interfaces", netInterfaceID}, "/")
 }
 
 func (v vpcService) itemPath(id string) string {
@@ -181,4 +242,84 @@ func (v vpcService) Delete(ctx context.Context, vpcID string) error {
 		return err
 	}
 	return resp.Body.Close()
+}
+
+func (v vpcService) CreateVPCNetworkInterface(ctx context.Context, networkID string, cnip *CreateNetworkInterfacePayload) (*NetworkInterface, error) {
+	req, err := v.client.NewRequest(ctx, http.MethodPost, serverServiceName, v.resourceCreateVPCNetworkInterfacePath(networkID), cnip)
+	if err != nil {
+		return nil, err
+	}
+	var data *NetworkInterface
+	resp, err := v.client.Do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (v vpcService) GetVPCNetworkInterface(ctx context.Context, networkID string, netInterfaceID string) (*NetworkInterface, error) {
+	req, err := v.client.NewRequest(ctx, http.MethodGet, serverServiceName, v.resourceVPCNetworkInterfacePath(networkID, netInterfaceID), nil)
+	if err != nil {
+		return nil, err
+	}
+	var data *NetworkInterface
+	resp, err := v.client.Do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (v vpcService) UpdateVPCNetworkInterface(ctx context.Context, networkID string, netInterfaceID string, unip *UpdateNetworkInterfacePayload) (*NetworkInterface, error) {
+	req, err := v.client.NewRequest(ctx, http.MethodPut, serverServiceName, v.resourceVPCNetworkInterfacePath(networkID, netInterfaceID), unip)
+	if err != nil {
+		return nil, err
+	}
+	var data *NetworkInterface
+	resp, err := v.client.Do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (v vpcService) DeleteVPCNetworkInterface(ctx context.Context, networkID string, netInterfaceID string) error {
+	req, err := v.client.NewRequest(ctx, http.MethodDelete, serverServiceName, v.resourceVPCNetworkInterfacePath(networkID, netInterfaceID), nil)
+	if err != nil {
+		return err
+	}
+	resp, err := v.client.Do(ctx, req)
+	if err != nil {
+		return err
+	}
+	return resp.Body.Close()
+}
+
+func (v vpcService) ListNetworkInterface(ctx context.Context) ([]*NetworkInterface, error) {
+	req, err := v.client.NewRequest(ctx, http.MethodGet, serverServiceName, "/network-interfaces", nil)
+	if err != nil {
+		return nil, err
+	}
+	var data []*NetworkInterface
+	resp, err := v.client.Do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return data, nil
 }
--- vpc_test.go
+++ vpc_test.go
@@ -528,3 +528,280 @@ func TestVPCDelete(t *testing.T) {
 		})
 	require.NoError(t, client.VPC.Delete(ctx, "0e03c7c5-267b-41f9-baa7-c4d2f2283d50"))
 }
+
+func TestNetworkInterfaceList(t *testing.T) {
+	setup()
+	defer teardown()
+	mux.HandleFunc(testlib.CloudServerURL("/network-interfaces"), func(writer http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		resp := `[
+    {
+        "id": "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34",
+        "name": "test-1",
+        "network_id": "99b82e5d-98c3-403f-9ff5-8b5b940e3665",
+        "tenant_id": "7c759790478644f88e7a58fca8dc6658",
+        "mac_address": "fa:16:3e:98:25:bc",
+        "admin_state_up": true,
+        "status": "DOWN",
+        "device_id": "",
+        "device_owner": "",
+        "fixed_ips": [
+            {
+                "subnet_id": "cb287da4-a2ee-4ad8-8287-4f070c359ae9",
+                "ip_address": "10.20.1.80"
+            }
+        ],
+        "allowed_address_pairs": [],
+        "extra_dhcp_opts": [],
+        "security_groups": [
+            "e87c2f6c-32ec-43d7-b27e-804c4b463238"
+        ],
+        "description": "",
+        "binding:vnic_type": "normal",
+        "port_security_enabled": true,
+        "qos_policy_id": null,
+        "tags": [],
+        "created_at": "2021-07-12T08:55:31Z",
+        "updated_at": "2021-07-12T08:55:31Z",
+        "revision_number": 1,
+        "project_id": "7c759790478644f88e7a58fca8dc6658",
+        "attached_server": {},
+        "firewalls": []
+    },
+    {
+        "id": "f8f78df1-43f1-4c73-9f4c-7d64fecb3b35",
+        "name": "test-2",
+        "network_id": "99b82e5d-98c3-403f-9ff5-8b5b940e3665",
+        "tenant_id": "7c759790478644f88e7a58fca8dc6658",
+        "mac_address": "fa:16:3e:98:25:bc",
+        "admin_state_up": true,
+        "status": "DOWN",
+        "device_id": "",
+        "device_owner": "",
+        "fixed_ips": [
+            {
+                "subnet_id": "cb287da4-a2ee-4ad8-8287-4f070c359ae9",
+                "ip_address": "10.20.1.80"
+            }
+        ],
+        "allowed_address_pairs": [],
+        "extra_dhcp_opts": [],
+        "security_groups": [
+            "e87c2f6c-32ec-43d7-b27e-804c4b463238"
+        ],
+        "description": "",
+        "binding:vnic_type": "normal",
+        "port_security_enabled": true,
+        "qos_policy_id": null,
+        "tags": [],
+        "created_at": "2021-07-12T08:55:31Z",
+        "updated_at": "2021-07-12T08:55:31Z",
+        "revision_number": 1,
+        "project_id": "7c759790478644f88e7a58fca8dc6658",
+        "attached_server": {},
+        "firewalls": []
+    },
+    {
+        "id": "f8f78df1-43f1-4c73-9f4c-7d64fecb3b36",
+        "name": "test-3",
+        "network_id": "99b82e5d-98c3-403f-9ff5-8b5b940e3665",
+        "tenant_id": "7c759790478644f88e7a58fca8dc6658",
+        "mac_address": "fa:16:3e:98:25:bc",
+        "admin_state_up": true,
+        "status": "DOWN",
+        "device_id": "",
+        "device_owner": "",
+        "fixed_ips": [
+            {
+                "subnet_id": "cb287da4-a2ee-4ad8-8287-4f070c359ae9",
+                "ip_address": "10.20.1.80"
+            }
+        ],
+        "allowed_address_pairs": [],
+        "extra_dhcp_opts": [],
+        "security_groups": [
+            "e87c2f6c-32ec-43d7-b27e-804c4b463238"
+        ],
+        "description": "",
+        "binding:vnic_type": "normal",
+        "port_security_enabled": true,
+        "qos_policy_id": null,
+        "tags": [],
+        "created_at": "2021-07-12T08:55:31Z",
+        "updated_at": "2021-07-12T08:55:31Z",
+        "revision_number": 1,
+        "project_id": "7c759790478644f88e7a58fca8dc6658",
+        "attached_server": {},
+        "firewalls": []
+    }
+]
+`
+		_, _ = fmt.Fprint(writer, resp)
+	})
+	networkInterfaces, err := client.VPC.ListNetworkInterface(ctx)
+	require.NoError(t, err)
+	assert.Len(t, networkInterfaces, 3)
+	assert.Equal(t, 1, networkInterfaces[0].RevisionNumber)
+}
+
+func TestVPCNetworkInterfaceCreate(t *testing.T) {
+	setup()
+	defer teardown()
+	var v vpcService
+	mux.HandleFunc(testlib.CloudServerURL(v.resourceCreateVPCNetworkInterfacePath("99b82e5d-98c3-403f-9ff5-8b5b940e3665")), func(writer http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		var payload *CreateNetworkInterfacePayload
+		require.NoError(t, json.NewDecoder(r.Body).Decode(&payload))
+		assert.Equal(t, "test", payload.Name)
+		resp := `{
+            "id": "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34",
+            "name": "test",
+            "network_id": "99b82e5d-98c3-403f-9ff5-8b5b940e3665",
+            "tenant_id": "7c759790478644f88e7a58fca8dc6658",
+            "mac_address": "fa:16:3e:98:25:bc",
+            "admin_state_up": true,
+            "status": "DOWN",
+            "device_id": "",
+            "device_owner": "",
+            "fixed_ips": [
+                {
+                    "subnet_id": "cb287da4-a2ee-4ad8-8287-4f070c359ae9",
+                    "ip_address": "10.20.1.80"
+                }
+            ],
+            "allowed_address_pairs": [],
+            "extra_dhcp_opts": [],
+            "security_groups": [
+                "e87c2f6c-32ec-43d7-b27e-804c4b463238"
+            ],
+            "description": "",
+            "binding:vnic_type": "normal",
+            "port_security_enabled": true,
+            "qos_policy_id": null,
+            "tags": [],
+            "created_at": "2021-07-12T08:55:31Z",
+            "updated_at": "2021-07-12T08:55:31Z",
+            "revision_number": 1,
+            "project_id": "7c759790478644f88e7a58fca8dc6658",
+            "attached_server": {},
+            "firewalls": []
+        }`
+		_, _ = fmt.Fprint(writer, resp)
+	})
+	networkInterface, err := client.VPC.CreateVPCNetworkInterface(ctx, "99b82e5d-98c3-403f-9ff5-8b5b940e3665", &CreateNetworkInterfacePayload{
+		Name: "test",
+	})
+	require.NoError(t, err)
+	assert.Equal(t, "test", networkInterface.Name)
+	assert.Equal(t, true, networkInterface.AdminStateUp)
+}
+
+func TestVPCNetworkInterfaceGet(t *testing.T) {
+	setup()
+	defer teardown()
+	var v vpcService
+	mux.HandleFunc(testlib.CloudServerURL(v.resourceVPCNetworkInterfacePath("99b82e5d-98c3-403f-9ff5-8b5b940e3665", "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34")),
+		func(writer http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodGet, r.Method)
+			resp := `{
+                "id": "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34",
+                "name": "test_update",
+                "network_id": "99b82e5d-98c3-403f-9ff5-8b5b940e3665",
+                "tenant_id": "7c759790478644f88e7a58fca8dc6658",
+                "mac_address": "fa:16:3e:98:25:bc",
+                "admin_state_up": true,
+                "status": "DOWN",
+                "device_id": "",
+                "device_owner": "",
+                "fixed_ips": [
+                    {
+                        "subnet_id": "cb287da4-a2ee-4ad8-8287-4f070c359ae9",
+                        "ip_address": "10.20.1.80"
+                    }
+                ],
+                "allowed_address_pairs": [],
+                "extra_dhcp_opts": [],
+                "security_groups": [
+                    "e87c2f6c-32ec-43d7-b27e-804c4b463238"
+                ],
+                "description": "",
+                "binding:vnic_type": "normal",
+                "port_security_enabled": true,
+                "qos_policy_id": null,
+                "tags": [],
+                "created_at": "2021-07-12T08:55:31Z",
+                "updated_at": "2021-07-12T08:55:31Z",
+                "revision_number": 1,
+                "project_id": "7c759790478644f88e7a58fca8dc6658",
+                "attached_server": {},
+                "firewalls": []
+            }`
+			_, _ = fmt.Fprint(writer, resp)
+		})
+	vpc, err := client.VPC.GetVPCNetworkInterface(ctx, "99b82e5d-98c3-403f-9ff5-8b5b940e3665", "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34")
+	require.NoError(t, err)
+	assert.Equal(t, "2021-07-12T08:55:31Z", vpc.CreatedAt)
+}
+
+func TestVPCNetworkInterfaceUpdate(t *testing.T) {
+	setup()
+	defer teardown()
+	var v vpcService
+	mux.HandleFunc(testlib.CloudServerURL(v.resourceVPCNetworkInterfacePath("99b82e5d-98c3-403f-9ff5-8b5b940e3665", "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34")),
+		func(writer http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodPut, r.Method)
+			var payload *UpdateNetworkInterfacePayload
+			require.NoError(t, json.NewDecoder(r.Body).Decode(&payload))
+			assert.Equal(t, "test_update", payload.Name)
+			resp := `{
+                "id": "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34",
+                "name": "test_update",
+                "network_id": "99b82e5d-98c3-403f-9ff5-8b5b940e3665",
+                "tenant_id": "7c759790478644f88e7a58fca8dc6658",
+                "mac_address": "fa:16:3e:98:25:bc",
+                "admin_state_up": true,
+                "status": "DOWN",
+                "device_id": "",
+                "device_owner": "",
+                "fixed_ips": [
+                    {
+                        "subnet_id": "cb287da4-a2ee-4ad8-8287-4f070c359ae9",
+                        "ip_address": "10.20.1.80"
+                    }
+                ],
+                "allowed_address_pairs": [],
+                "extra_dhcp_opts": [],
+                "security_groups": [
+                    "e87c2f6c-32ec-43d7-b27e-804c4b463238"
+                ],
+                "description": "",
+                "binding:vnic_type": "normal",
+                "port_security_enabled": true,
+                "qos_policy_id": null,
+                "tags": [],
+                "created_at": "2021-07-12T08:55:31Z",
+                "updated_at": "2021-07-12T08:55:31Z",
+                "revision_number": 1,
+                "project_id": "7c759790478644f88e7a58fca8dc6658",
+                "attached_server": {},
+                "firewalls": []
+            }`
+			_, _ = fmt.Fprint(writer, resp)
+		})
+	networkInterface, err := client.VPC.UpdateVPCNetworkInterface(ctx, "99b82e5d-98c3-403f-9ff5-8b5b940e3665", "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34", &UpdateNetworkInterfacePayload{
+		Name: "test_update",
+	})
+	require.NoError(t, err)
+	assert.Equal(t, "test_update", networkInterface.Name)
+}
+
+func TestVPCNetworkInterfaceDelete(t *testing.T) {
+	setup()
+	defer teardown()
+	var v vpcService
+	mux.HandleFunc(testlib.CloudServerURL(v.resourceVPCNetworkInterfacePath("99b82e5d-98c3-403f-9ff5-8b5b940e3665", "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34")),
+		func(writer http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodDelete, r.Method)
+		})
+	require.NoError(t, client.VPC.DeleteVPCNetworkInterface(ctx, "99b82e5d-98c3-403f-9ff5-8b5b940e3665", "f8f78df1-43f1-4c73-9f4c-7d64fecb3b34"))
+}
