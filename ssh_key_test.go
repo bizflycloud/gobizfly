62c828751014ec506a340a45d78e40bd6dfc07d1
Tùng Đỗ Sơn <svtt.tungds@vccloud.vn>
2021-11-09 10:06:27 +0700

    feat: add keypair GET func

--- ssh_key_test.go
+++ ssh_key_test.go
@@ -91,20 +91,26 @@ func TestSSHKeyGet(t *testing.T) {
 	setup()
 	defer teardown()
 
-	mux.HandleFunc(testlib.CloudServerURL(sshKeyBasePath + "/sapd1"), func(writer http.ResponseWriter, request *http.Request) {
+	mux.HandleFunc(testlib.CloudServerURL(sshKeyBasePath + "/lam621"), func(writer http.ResponseWriter, request *http.Request) {
 		require.Equal(t, http.MethodGet, request.Method)
 		resp := `
-    {
-            "name": "sapd1",
-            "public_key": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgXX0Kdd3XKojgj7maVd3PsPApzh9n2lT2CtgcJs8jw9i3mit5SZu02QFS772Pa9VdGeSjbqxtADLRpnuigW5ii0dHBQTgWqx593Cs7QKRhyRPb88u0TFCZynRwfMRnb6qngiKoWp5TtaHuIY+7kS8SyqNVIwoCYlr9a4ePX8rwydf9crhJocgKb2LgQkdW3TBE5QAvxbruYlj201jjXFeE5BtE4QER0QyY5MqW8MAgG98N3w95pKIffhHZ4TO4A3zgpWbNn1ROproZgV+9COzZ7WYuvPWqWdLAntd9b1/lLnDrDHXa/lrefJXJVamhz4i1cfIZ/p+aFWG0a7DpL5b saphi@saphi-kma\n",
-            "fingerprint": "28:56:9e:4b:bb:a0:91:71:42:37:40:a2:d0:66:24:17"
-    }
-]
+{
+    "name": "lam621",
+    "public_key": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDcY38BoKy2Pqt+Nezm69hAnrjtw1b+bpOhtLjj1UypBJNrCjQGFJvG2rVgphWeUlTWjK7Jv8uqKjRGrlHyWgRK+qj56Q3P13BMcIJE4t1OOLXnsZO9Lv1J6RjtLoSgc3W+rBMLqyCkhlr6eca2YBbm/52yYhND32YevDiUky621Fdr5tbFjBEXcLYWOIJhmAAE5Y2Pe+7zpL0Ug7d8XHvJV7lsO4sqiDCvFq9SBYzbGIcJyxaabZ57aw8LO78y6LCzadK+xtLf8XSQXoF53d8ygx+nNyIEKqHfdX1BTSTQHAE5gXpwrcMSkIahTZmzOEQr3UX3J0NUKk/nK1/7Tk1 Generated-by-Nova",
+    "fingerprint": "43:a0:26:b7:6a:f9:26:94:8a:83:a0:90:a0:cb:cb:fa",
+    "created_at": "2021-02-03T13:01:45.000000",
+    "deleted": false,
+    "deleted_at": null,
+    "id": 49414,
+    "user_id": "7156c45b82cb4fabba997a90b032c0de",
+    "updated_at": null,
+    "type": "ssh"
+}
 `
 		_, _ = fmt.Fprint(writer, resp)
 	})
-	sshkey, err := client.SSHKey.Get(ctx, "sapd1")
+	sshkey, err := client.SSHKey.Get(ctx, "lam621")
 	require.NoError(t, err)
-	assert.Equal(t, "sapd1", sshkey.Name)
-	assert.Equal(t, "28:56:9e:4b:bb:a0:91:71:42:37:40:a2:d0:66:24:17", sshkey.FingerPrint)
+	assert.Equal(t, "lam621", sshkey.Name)
+	assert.Equal(t, "43:a0:26:b7:6a:f9:26:94:8a:83:a0:90:a0:cb:cb:fa", sshkey.FingerPrint)
 }
