f9b6aeb2a60780156162c7a83a83c8bbd4c7930b
Tùng Đỗ Sơn <svtt.tungds@vccloud.vn>
2022-01-19 22:28:26 +0700

    feat: add cloud backup activities

---
+++ cloud_backup_activities.go
@@ -0,0 +1,43 @@
+package gobizfly
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+type Activity struct {
+	Action            string `json:"action"`
+	BackupDirectoryId string `json:"backup_directory_id"`
+	CreatedAt         string `json:"created_at"`
+	Id                string `json:"id"`
+	Extra             string `json:"extra,omitempty"`
+	MachineId         string `json:"machine_id"`
+	Message           string `json:"message"`
+	PolicyId          string `json:"policy_id,omitempty"`
+	Reason            string `json:"reason,omitempty"`
+	RecoveryPoint     string `json:"recovery_point,omitempty"`
+	Status            string `json:"status"`
+	TenantId          string `json:"tenant_id"`
+	UpdatedAt         string `json:"updated_at"`
+	UserId            string `json:"user_id"`
+}
+
+func (cb *cloudBackupService) CloudBackupActivity(ctx context.Context) ([]*Activity, error) {
+	req, err := cb.client.NewRequest(ctx, http.MethodGet, cloudBackupServiceName, cb.activityPath(), nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := cb.client.Do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var data struct {
+		ActivityData []*Activity `json:"activities"`
+	}
+	if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return data.ActivityData, nil
+}
