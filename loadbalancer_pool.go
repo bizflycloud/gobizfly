26eb7c27e6161086f39b1d2af41d239a2d04f437
Tùng Đỗ Sơn <svtt.tungds@vccloud.vn>
2022-04-28 08:37:30 +0700

    feat: remove description field in create pool payload

--- loadbalancer_pool.go
+++ loadbalancer_pool.go
@@ -31,9 +31,8 @@ type SessionPersistence struct {
 
 // PoolCreateRequest represents create new pool request payload.
 type PoolCreateRequest struct {
-	Description        *string             `json:"description,omitempty"`
 	LBAlgorithm        string              `json:"lb_algorithm"`
-	ListenerID         *string             `json:"listener_id,omitempty"`
+	ListenerID         string              `json:"listener_id"`
 	Name               *string             `json:"name,omitempty"`
 	Protocol           string              `json:"protocol"`
 	SessionPersistence *SessionPersistence `json:"session_persistence,omitempty"`
