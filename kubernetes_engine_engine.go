69bc1e260e1697e4d71e97676c2bc7dc33901404
vochidat0 <dat.usth2000@gmail.com>
2023-09-29 11:31:04 +0700

    feat: replace worker config api by api list cluster's info by pool_id

--- kubernetes_engine_engine.go
+++ kubernetes_engine_engine.go
@@ -1,6 +1,7 @@
 package gobizfly
 
 import (
+	"io"
 	"context"
 	"encoding/json"
 	"net/http"
@@ -66,3 +67,45 @@ func (c *kubernetesEngineService) AddClusterEverywhere(ctx context.Context, id s
 	}
 	return joinEverywhereResponse, nil
 }
+
+type WorkerConfig struct {
+	ID               	string		`json:"id" yaml:"id"`
+	Version          	string 		`json:"version" yaml:"version"`
+	Everywhere       	bool      `json:"everywhere" yaml:"everywhere"`
+	Nvidiadevice			bool			`json:"nvidiadevice" yaml:"nvidiadevice"`
+	CniVersion        string    `json:"CNI_VERSION" yaml:"CNI_VERSION"`
+	RuncVersion       string    `json:"RUNC_VERSION" yaml:"RUNC_VERSION"`
+	ContainerdVersion string    `json:"CONTAINERD_VERSION" yaml:"CONTAINERD_VERSION"`
+	KubeVersion    		string   	`json:"KUBE_VERSION" yaml:"KUBE_VERSION"`
+
+}
+
+type ClusterInfoResponse struct {
+	WorkerConfig 	WorkerConfig 	`json:"worker_config" yaml:"worker_configs"`
+	K8sVersion		string			 	`json:"k8s_version" yaml:"k8s_version"`
+	ShootUid			string 				`json:"shoot_uid" yaml:"shoot_uid"`
+}
+
+func (c *kubernetesEngineService) GetClusterInfo(ctx context.Context, pool_id string) (*ClusterInfoResponse, error) {
+	var clusterInfo ClusterInfoResponse
+	req, err := c.client.NewRequest(ctx, http.MethodGet, kubernetesServiceName, ClusterInfo+"/"+pool_id, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.client.Do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(body, &clusterInfo)
+	if err != nil {
+		return nil, err
+	}
+	return &clusterInfo, nil
+}
+
