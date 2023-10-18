2da672f49201a24b88020f91352547441c1e8d3f
vochidat0 <dat.usth2000@gmail.com>
2023-10-18 11:13:01 +0700

    change: log resp.StatusCode == http.StatusUnauthorized

--- client.go
+++ client.go
@@ -255,6 +255,7 @@ func (c *Client) Do(ctx context.Context, req *http.Request) (resp *http.Response
 	}
 
 	fmt.Println("First Response status_code: ", resp.StatusCode)
+	fmt.Println("Verify status unauthorized: ", resp.StatusCode == http.StatusUnauthorized)
 	// If 401, get new token and retry one time.
 	if resp.StatusCode == http.StatusUnauthorized {
 		tok, tokErr := c.Token.Refresh(ctx)
