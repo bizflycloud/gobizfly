a7ce5ddc6932e17ed1f9ee519d2db5bd3df524bb
vochidat0 <dat.usth2000@gmail.com>
2023-10-19 09:59:12 +0700

    change: print status code

--- client.go
+++ client.go
@@ -266,6 +266,7 @@ func (c *Client) Do(ctx context.Context, req *http.Request) (resp *http.Response
 	err = retry(3, 2*time.Second,
 		func() error {
 			resp, err = c.do(ctx, req)
+			fmt.Println("Status code: ", resp.StatusCode)
 			if err != nil {
 				return err
 			}
