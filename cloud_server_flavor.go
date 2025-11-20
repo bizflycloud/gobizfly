0afb6f8b2fbe65a71b09e74ea912efd95f1cd5a0
QuanLM <quanlm1999@gmail.com>
2025-11-20 11:18:54 +0700

    Update list flavor to list new flavor with cpu vendor

--- cloud_server_flavor.go
+++ cloud_server_flavor.go
@@ -1,43 +1,76 @@
 package gobizfly
 
 import (
-	"context"
-	"encoding/json"
-	"net/http"
+    "context"
+    "encoding/json"
+    "net/http"
 )
 
-// serverFlavorResponse is the response from the server flavor API.
-type serverFlavorResponse struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"` // Deprecated: Will be removed in the future.
-	VCPUs    int    `json:"vcpus"`
-	RAM      int    `json:"ram"`
-	Disk     int    `json:"disk"`
-	Category string `json:"category"`
+// ServerFlavorResponse represents a server flavor from the API
+type ServerFlavorResponse struct {
+    ID           string           `json:"id"`
+    Name         string           `json:"name"`
+    RAM          int              `json:"ram"`           // RAM in MB
+    Disk         int              `json:"disk"`          // Disk in GB
+    Swap         string           `json:"swap"`
+    VCPUs        int              `json:"vcpus"`
+    GenerationID string           `json:"generation_id"`
+    Generation   FlavorGeneration `json:"generation"`
+    Category     string           `json:"category"`      // premium, basic, enterprise, etc.
+    GPU          *FlavorGPU       `json:"gpu"`           // Nullable
+    BillingPlans []string         `json:"billing_plans"`
+    IsNew        bool             `json:"is_new"`
+}
+
+// FlavorGeneration represents hardware generation information
+type FlavorGeneration struct {
+    ID                string   `json:"id"`
+    Name              string   `json:"name"`
+    Code              string   `json:"code"`
+    Category          string   `json:"category"`
+    Vendor            string   `json:"vendor"`
+    Model             string   `json:"model"`
+    Group             string   `json:"group"`
+    Description       string   `json:"description"`
+    AvailabilityZones []string `json:"availability_zones"`
+    Icon              string   `json:"icon"`
+}
+
+// FlavorGPU represents GPU information
+type FlavorGPU struct {
+    Name  string `json:"name"`
+    Count int    `json:"count"`
 }
 
 type cloudServerFlavorResource struct {
-	client *Client
+    client *Client
 }
 
 func (cs *cloudServerService) Flavors() *cloudServerFlavorResource {
-	return &cloudServerFlavorResource{client: cs.client}
+    return &cloudServerFlavorResource{client: cs.client}
 }
 
 // List lists server flavors
-func (s *cloudServerFlavorResource) List(ctx context.Context) ([]*serverFlavorResponse, error) {
-	req, err := s.client.NewRequest(ctx, http.MethodGet, serverServiceName, flavorPath, nil)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := s.client.Do(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-	var flavors []*serverFlavorResponse
-
-	if err := json.NewDecoder(resp.Body).Decode(&flavors); err != nil {
-		return nil, err
-	}
-	return flavors, nil
+func (s *cloudServerFlavorResource) List(ctx context.Context) ([]*ServerFlavorResponse, error) {
+    req, err := s.client.NewRequest(ctx, http.MethodGet, serverServiceName, flavorPath, nil)
+    if err != nil {
+        return nil, err
+    }
+    
+    q := req.URL.Query()
+    q.Set("new", "true")
+    req.URL.RawQuery = q.Encode()
+    
+    resp, err := s.client.Do(ctx, req)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+    
+    var flavors []*ServerFlavorResponse
+    if err := json.NewDecoder(resp.Body).Decode(&flavors); err != nil {
+        return nil, err
+    }
+    
+    return flavors, nil
 }
